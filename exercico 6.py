db376e7e458f6b4f9c93dd33c797296f9971f795
Marcos Augusto <marcosarpc@al.insper.edu.br>
2023-05-02 14:45:53 -0300

    5tg5g66bh

--- exercico 6.py
+++ exercico 6.py
@@ -1,5 +1,25 @@
-def posicoao_valida(frota, linha, coluna, orientacao, tamanho):
-    posicoao_navio = define_posicao (linha, coluna, orientacao, tamanho)
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha+i, coluna])
+    elif orientacao == "horizontal":
+        for i in range(tamanho):
+            posicoes.append([linha, coluna+i])
+    return posicoes
 
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    posicoes_navio = define_posicoes(linha, coluna, orientacao, tamanho)
 
-    for navio in frota.valeus()
+ 
+    for navio in frota.values():
+        for posicoes_ocupadas in navio:
+            for posicao in posicoes_navio:
+                if posicao in posicoes_ocupadas:
+                    return False
+
+    for posicao in posicoes_navio:
+        if posicao[0] < 0 or posicao[0] > 9 or posicao[1] < 0 or posicao[1] > 9:
+            return False
+    
+    return True
