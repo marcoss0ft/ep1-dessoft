eb4c2a11ae01076c2a3234a09a7fd4dcdd0a12f8
Lucca Nicoletti <luccafn1@al.insper.edu.br>
2023-05-02 14:44:49 -0300

    u9iju9j

--- exercico 6.py
+++ exercico 6.py
@@ -1,25 +1,5 @@
-def define_posicoes(linha, coluna, orientacao, tamanho):
-    posicoes = []
-    if orientacao == "vertical":
-        for i in range(tamanho):
-            posicoes.append([linha+i, coluna])
-    elif orientacao == "horizontal":
-        for i in range(tamanho):
-            posicoes.append([linha, coluna+i])
-    return posicoes
+def posicoao_valida(frota, linha, coluna, orientacao, tamanho):
+    posicoao_navio = define_posicao (linha, coluna, orientacao, tamanho)
 
-def posicao_valida(frota, linha, coluna, orientacao, tamanho):
-    posicoes_navio = define_posicoes(linha, coluna, orientacao, tamanho)
 
- 
-    for navio in frota.values():
-        for posicoes_ocupadas in navio:
-            for posicao in posicoes_navio:
-                if posicao in posicoes_ocupadas:
-                    return False
-
-    for posicao in posicoes_navio:
-        if posicao[0] < 0 or posicao[0] > 9 or posicao[1] < 0 or posicao[1] > 9:
-            return False
-    
-    return True
+    for navio in frota.valeus()
