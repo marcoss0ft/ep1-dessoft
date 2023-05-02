7fd92a7d952ca0dd414f31a90dd15ed5012795d9
Lucca Nicoletti <luccafn1@al.insper.edu.br>
2023-05-02 13:46:01 -0300

    COMIT

--- Exercicio4.py
+++ Exercicio4.py
@@ -1,1 +1,7 @@
-def
+def posiciona_frota(frota):
+    grid = [[0]*10 for _ in range(10)]
+    for tipo_navio, posicoes in frota.items():
+        for posicao in posicoes:
+            for linha, coluna in posicao:
+                grid[linha][coluna] = 1
+    return grid
