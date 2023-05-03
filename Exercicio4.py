5ddbb9f6c872457aa0d81fa423dcd3414fa48fa1
Lucca Nicoletti <luccafn1@al.insper.edu.br>
2023-05-03 09:35:26 -0300

    correcao

--- Exercicio4.py
+++ Exercicio4.py
@@ -1,7 +1,8 @@
 def posiciona_frota(frota):
-    grid = [[0]*10 for _ in range(10)]
-    for tipo_navio, posicoes in frota.items():
+    grid = [[0 for _ in range(10)] for _ in range(10)] 
+    for navio, posicoes in frota.items(): 
         for posicao in posicoes:
             for linha, coluna in posicao:
                 grid[linha][coluna] = 1
     return grid
+
