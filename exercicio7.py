c4a9853b0b177751bf781881125f45b6ee8a5110
Marcos Augusto <marcosarpc@al.insper.edu.br>
2023-05-03 11:58:46 -0300

    ex7 versao beta

--- exercicio7.py
+++ exercicio7.py
@@ -1,11 +1,62 @@
-navios = ['porta-aviões','navio-tanque','contratorpedeiro','submarino']
-tamanhos = [4,3,2,1]
-quantidades = [1,2,3,4]
+navios = {'porta-aviões':[1,4],'navio-tanque':[2,3],'contratorpedeiro':[3,2],'submarino':[4,1]}
 i = 0
+frota = {}
+
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha+i, coluna])
+    elif orientacao == "horizontal":
+        for i in range(tamanho):
+            posicoes.append([linha, coluna+i])
+    return posicoes
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    
+
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else:
+        frota[nome_navio] = [posicoes]
+    
+    return frota
+
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    posicoes_navio = define_posicoes(linha, coluna, orientacao, tamanho)
+
+ 
+    for navio in frota.values():
+        for posicoes_ocupadas in navio:
+            for posicao in posicoes_navio:
+                if posicao in posicoes_ocupadas:
+                    return False
+
+    for posicao in posicoes_navio:
+        if posicao[0] < 0 or posicao[0] > 9 or posicao[1] < 0 or posicao[1] > 9:
+            return False
+    
+    return True
+
+
 for navio in navios:
-    print('\nInsira as informações referentes ao navio {0} que possui tamanho {1};\n'.format(navio,tamanhos[i]))
-    for n in range (quantidades[i]):
+    quantidade = navios[navio][0]
+    tamanho = navios[navio][1]
+    print('\nInsira as informações referentes ao navio {0} que possui tamanho {1};\n'.format(navio,tamanho))
+    for n in range (quantidade):
         linha = int(input('\nQual linha?\n'))
         coluna = int(input('\nQual coluna?\n'))
         if navio != 'submarino':
-            orientacao = int(input('\nQual orientacao?\n'))
+            orientacao = input('\nQual orientacao?\n')
+        else:
+            orientacao = 'vertical'
+        validacao = posicao_valida(frota,linha,coluna,orientacao,tamanho)
+        if validacao == True:
+            frota = preenche_frota(frota,navio,linha,coluna,orientacao,tamanho)
+        else:
+            print('\nEsta posição não está válida!\n')
+
+
+print(frota)
